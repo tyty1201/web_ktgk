5af89bb5604be860811e5c98843420d5e3dc928e
thiistired <qthiiilt@gmail.com>
2026-04-14 08:51:07 +0700

    Fix routes profile and account + layout updates

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index']);
@@ -9,6 +10,14 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-
+// Profile Routes
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/account', function () {
+        return view('account');        // hoặc bạn sẽ tạo controller sau
+    })->name('account');
+});
 
 require __DIR__.'/auth.php';
