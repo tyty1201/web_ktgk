7322c2d52a895b726bf3549b22d4e4588272ec8b
thiistired <qthiiilt@gmail.com>
2026-04-14 09:08:19 +0700

    đăng nhập đăng kí

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [HomeController::class, 'index']);
+Route::get('/', [HomeController::class, 'index'])->name('home');   // ← Quan trọng: thêm name('home')
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -15,8 +15,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
     Route::get('/account', function () {
-        return view('account');        // hoặc bạn sẽ tạo controller sau
+        return view('account');   // tạm thời, sau bạn sẽ thay bằng controller
     })->name('account');
 });
 
